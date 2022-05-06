d552a5d1954ff0c1b1b78a4f7059903db2fd2245
Emanoel Brunio <emanoelbrunio@gmail.com>
2022-05-05 21:27:54 -0300

    add questao 37

--- questao37/Cliente.java
+++ questao37/Cliente.java
@@ -1,4 +1,5 @@
 package questao37;
+import java.util.ArrayList;
 /* Crie um objeto de uma classe chamada Cliente com os atributos id, nome, idade, telefone. Faça um programa para solicitar os dados de vários clientes e armazenar em um ArrayList até que se digite um número de ID negativo. Em seguida, exiba os dados de todos os clientes.
 */
 public class Cliente {
@@ -6,6 +7,31 @@ public class Cliente {
     private String nome;
     private int idade;
     private int telefone;
+
+    ArrayList<Cliente> lista;
+    
+    Cliente(int id, String nome, int idade, int telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+    }
+    
+    // new ArrayList<>;
+
+    public String toString() {
+		String result = "";
+
+        result += ("ID: " + this.id + "\n");
+
+        result += ("Nome: " + this.nome + "\n");
+
+        result += ("Idade: " + this.idade + "\n");
+        
+        result += ("Telefone: " + this.telefone + "\n\n");
+
+		return result;
+	}
 
     
 }
--- questao37/principal.java
+++ questao37/principal.java
@@ -1,9 +1,57 @@
 package questao37;
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class principal {
     public static void main(String[] args) {
         
+        ArrayList<Cliente> lista = new ArrayList<>();
+
+        Scanner ent = new Scanner(System.in);
+        Scanner ent1 = new Scanner(System.in);
+
+        int i = 0;
+        
+        int id;
+        do {
+            int idade, telefone;
+            String nome;
+            System.out.printf("Cliente: %d\n", i+1);
+            
+            System.out.printf("ID: ");
+            id = ent.nextInt();
+
+            // se quiser receber um cliente com id negativo é só remover este if
+            if (id < 0) {
+                break;
+            }
+
+            System.out.printf("Nome: ");
+            nome = ent1.nextLine();
+
+            System.out.printf("Idade: ");
+            idade = ent.nextInt();
+
+            System.out.printf("Telefone: ");
+            telefone = ent.nextInt();
+            
+            lista.add(new Cliente(id, nome, idade, telefone));
+            
+            i++;
+            System.out.println();
+        } while (id >= 0);
+
+        System.out.println();
+        System.out.println("Dados de todos os clientes:");
+
+        int j = 0;
+        while(j < lista.size()){
+            System.out.printf("Cliente: %d\n", j+1);            
+            System.out.print(lista.get(j).toString());
+            j++;
+        }
+        
+        ent.close();
+        ent1.close();
     }
 }
